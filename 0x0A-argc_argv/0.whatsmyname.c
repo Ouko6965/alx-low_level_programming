80d40db12de5a66a276d802cfd88c94429e69842
Ouko David <Ouko.dave@gmail.com>
2022-11-09 11:06:53 -0800

    0.whatsmyname.c

--- 0x0A-argc_argv/0.whatsmyname.c
+++ 0x0A-argc_argv/0.whatsmyname.c
@@ -1,13 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main ->this is a function to print its name
+ * main - this is a function to print its name
  * @argc: argc parameter
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main (int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
-	return (o);
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
